80666ed1f98262f9e701d86f6f9ddb4cce126774
jaideep <jtibrewala@gmail.com>
2016-03-25 18:16:01 +0530

    Adding files to repository

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+var myImage = document.getElementById("home");
+    myImage.onclick =  function() {
+        console.log(document.getElementById("home").style.width);
+//        alert("You clicked the image");
+    };
